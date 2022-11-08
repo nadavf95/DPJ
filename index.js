937fe04a657a39a4172296cde6dc0da519e98efc
Nadav Fuchs <nadavf95@gmail.com>
2022-11-08 13:46:05 +0200

    toggle task

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ contactButton = document.querySelector("#contact button");
 contactButton.addEventListener("click", function contactClickhandler(el) {
   alert("Not implemented");
 });
+
+
+var divs = document.getElementsByClassName('toggle-living');
+
+function living_f(id) {
+    for (var i = 0; i < divs.length; i++) {
+      var div = divs[i];
+      if (div.id == id)
+        div.style.display = 'block';
+      else
+        div.style.display = 'none';
+    }
+}
